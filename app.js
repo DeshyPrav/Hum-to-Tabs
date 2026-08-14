51bcb03ca580a6c9efee9ea9075ebcd93764161d
DeshyPrav <adeesh.prav@gmail.com>
2026-08-14 16:53:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 console.log("app.js is running");
 
 function frequencyToNoteNumber(freq) {
-  return 12 * Math.log2(freq / 440) + 69;
+    return 12 * Math.log2(freq / 440) + 69;
 }
+
 console.log(frequencyToNoteNumber(440));
