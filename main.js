0d1734f2ea2b9df356f2cca14dd30646d61bac62
DeshyPrav <adeesh.prav@gmail.com>
2026-08-14 17:17:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,63 @@
-console.log("NEW VERSION");
+console.log("Hum to Tab is running");
 
+// Frequency to MIDI note number
 function frequencyToNoteNumber(freq) {
     return 12 * Math.log2(freq / 440) + 69;
 }
 
 console.log(frequencyToNoteNumber(440));
 
-let lowE = [
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-    "A",
-    "A#",
-    "B",
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E"
-];
 
-console.log(lowE[3]);
+// Guitar strings, frets 0–12
+
+let lowE = [
+    "E", "F", "F#", "G", "G#", "A", "A#",
+    "B", "C", "C#", "D", "D#", "E"
+];
 
 let A = [
-    "A",
-    "A#",
-    "B",
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-    "A"
+    "A", "A#", "B", "C", "C#", "D", "D#",
+    "E", "F", "F#", "G", "G#", "A"
 ];
-console.log(A[3]);
 
 let D = [
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-    "A",
-    "A#",
-    "B",
-    "C",
-    "C#",
-    "D"
+    "D", "D#", "E", "F", "F#", "G", "G#",
+    "A", "A#", "B", "C", "C#", "D"
 ];
 
-console.log(D[5]);
+let G = [
+    "G", "G#", "A", "A#", "B", "C", "C#",
+    "D", "D#", "E", "F", "F#", "G"
+];
+
+let B = [
+    "B", "C", "C#", "D", "D#", "E", "F",
+    "F#", "G", "G#", "A", "A#", "B"
+];
+
+let highE = [
+    "E", "F", "F#", "G", "G#", "A", "A#",
+    "B", "C", "C#", "D", "D#", "E"
+];
+
+
+// Put all strings into one guitar
+
+let guitar = {
+    "Low E": lowE,
+    "A": A,
+    "D": D,
+    "G": G,
+    "B": B,
+    "High E": highE
+};
+
+
+// Test the guitar
+
+console.log(guitar["Low E"][3]);
+console.log(guitar["A"][3]);
+console.log(guitar["D"][5]);
+console.log(guitar["G"][0]);
+console.log(guitar["B"][8]);
+console.log(guitar["High E"][3]);
